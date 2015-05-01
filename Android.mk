2158f7a4c2b57e21f1cd66bf16cf133625a3485c
Kai-Zhen Li <kli@mozilla.com>
2015-05-01 11:50:23 +0800

    Disable some unsupported modules.

--- Android.mk
+++ Android.mk
@@ -2,3 +2,9 @@
 # This empty Android.mk file exists to prevent the build system from
 # automatically including any other Android.mk files under this directory.
 #
+DISABLED_USER_MODULES += \
+	zipalign \
+	libdlext_test_fd_zipaligned \
+	binderAddInts \
+	backtrace_test \
+	dhcpcd_test
